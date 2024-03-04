cd3b8955502aa400d084dca46936ffe326268690
david sanchez <david.felipe.sanchez2@gmail.com>
2024-03-04 03:15:53 -0500

    nuevocambio

--- arrayAleatorio.js
+++ arrayAleatorio.js
@@ -1,8 +1,34 @@
+const { parse } = require('path');
 
-const animales = ["raton","perro","gato","hipopo"];
-const animalAleat = Math.floor(Math.random()* animales.length);
-const animal = animales[animalAleat];
+const rli = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+function input(prompt) {
+    return new Promise((callbackFn, errorFn) => {
+        rli.question(prompt, (uinput)=> {
+            callbackFn(uinput);
+        }, ()=> {
+            errorFn();
+        });
+    });
+}
+ 
 
+const main = async () => {
+    const numInicio = await input("Introduzca numero de inicio de su numero aleatorio : ");
+    const numFinal = await input("Introduzca el numero final del numero aleatorio : ");
+    const cantidadAleatorio = await input("Introduzca la cantidad de numeros aleaterios que deseas : ");
+    const listaNumeros = [];
+        for (let i = 0; i < cantidadAleatorio; i++) {
+           let numerosAleatorios = Math.floor(Math.random() * (parseFloat(numFinal) - parseFloat(numInicio) + 1)) + parseFloat(numInicio);
+           listaNumeros.push(numerosAleatorios);
+        }
+        console.log(listaNumeros);
+    
+   
+    rli.close();
+};
 
-console.log(animal);
+main();
