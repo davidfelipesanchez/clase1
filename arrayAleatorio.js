5fc86ef44767d00be02df1f42ea14503b7ae818c
david sanchez <david.felipe.sanchez2@gmail.com>
2024-03-04 02:50:50 -0500

    cambio1

---
+++ arrayAleatorio.js
@@ -0,0 +1,8 @@
+
+const animales = ["raton","perro","gato","hipopo"];
+const animalAleat = Math.floor(Math.random()* animales.length);
+const animal = animales[animalAleat];
+
+
+
+console.log(animal);
